512375d8225d673e12773993586aae4afd163444
t1dmlgus <dmlgusgngl@gmail.com>
2021-09-07 12:12:01 +0900

    백엔드 설정

---
+++ src/main/java/com/s1dmlgus/coffeeEx01/CoffeeEx01Application.java
@@ -0,0 +1,15 @@
+package com.s1dmlgus.coffeeEx01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeEx01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeEx01Application.class, args);
+
+		 
+	}
+
+}
---
+++ src/test/java/com/s1dmlgus/coffeeEx01/CoffeeEx01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1dmlgus.coffeeEx01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeEx01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
